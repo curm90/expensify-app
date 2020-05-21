cc4d349951e95f19c5e1fedcb35ae2f3fba68bfd
Liam Sutton <devliam90@gmail.com>
2020-05-21 11:45:45 +0100

    test selectors

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'water bill' }));
 store.dispatch(addExpense({ description: 'gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(
+  state.expenses,
+  state.filters
+);
+
+console.log(visibleExpenses);
 
 console.log(store.getState());
 
