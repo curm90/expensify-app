b1037d21ab6a8de6bee82ebcd39acf575f8b979b
Liam Sutton <devliam90@gmail.com>
2020-05-21 14:41:25 +0100

    dispatch setTextFilter to filter expenses

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFIlters from './ExpenseListFilters';
 
 const Homepage = () => (
   <div>
     <h1>Homepage</h1>
+    <ExpenseListFIlters />
     <ExpenseList />
   </div>
 );
