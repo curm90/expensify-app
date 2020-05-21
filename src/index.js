b57c7f530d8b68dc09311deff4557dcfce19beb2
Liam Sutton <devliam90@gmail.com>
2020-05-21 14:33:24 +0100

    show filtered expenses

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import getVisibleExpenses from './selectors/expense';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'water bill' }));
+store.dispatch(
+  addExpense({ description: 'water bill', amount: 4500 })
+);
 store.dispatch(addExpense({ description: 'gas bill' }));
 store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
-  store.dispatch(setTextFilter('rent'));
+  store.dispatch(setTextFilter('bill'));
 }, 3000);
 
 const state = store.getState();
