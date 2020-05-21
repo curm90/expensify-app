d2f5b949d907459b6421b830f6dc513dc8b76263
Liam Sutton <devliam90@gmail.com>
2020-05-21 11:30:07 +0100

    create store and log state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
