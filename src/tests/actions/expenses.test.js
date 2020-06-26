95de6781f6ffe2a8d4b79b5716c57008ac89df20
Liam Sutton <devliam90@gmail.com>
2020-06-26 14:33:21 +0100

    expense action tests

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,57 @@
+import {
+  addExpense,
+  editExpense,
+  removeExpense,
+} from '../../actions/expenses';
+import expense from '../../selectors/expense';
+
+it('Should set up remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc',
+  });
+});
+
+it('Should update expense with passed in updates', () => {
+  const action = editExpense('abc123', { note: 'New note value' });
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: 'abc123',
+    updates: {
+      note: 'New note value',
+    },
+  });
+});
+
+it('Should setup add expense action object with provided values', () => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 109500,
+    createdAt: 1000,
+    note: 'This was last months rent',
+  };
+
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String),
+    },
+  });
+});
+
+it('Should set up add expense action object with default values', () => {
+  const action = addExpense();
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      description: '',
+      amount: 0,
+      createdAt: 0,
+      note: '',
+      id: expect.any(String),
+    },
+  });
+});
