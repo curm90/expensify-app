9b1461f044899081fdf1ace0082b50724a796f4d
Liam Sutton <devliam90@gmail.com>
2020-06-26 13:01:44 +0100

    challenge: Link id to expense item

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({
@@ -10,7 +11,9 @@ const ExpenseListItem = ({
   dispatch,
 }) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>
       {amount} - {createdAt}
     </p>
