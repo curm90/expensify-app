9de81f30e78f748ebf22f0e0d5943deb199949e2
Liam Sutton <devliam90@gmail.com>
2020-06-26 13:19:15 +0100

    add remove button to expenses

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({
-  id,
-  description,
-  amount,
-  createdAt,
-  dispatch,
-}) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
@@ -17,14 +9,7 @@ const ExpenseListItem = ({
     <p>
       {amount} - {createdAt}
     </p>
-    <button
-      onClick={() => {
-        dispatch(removeExpense({ id }));
-      }}
-    >
-      Remove
-    </button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
