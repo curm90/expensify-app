724e66a2da0a1842551d639ef2fb25a9a2529c24
Liam Sutton <devliam90@gmail.com>
2020-07-01 17:26:07 +0100

    refacor edit expense to class component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import getVisibleExpenses from './selectors/expense';
 import 'normalize.css/normalize.css';
-import './styles/styles.css';
+import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
