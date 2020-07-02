21851ffa8b80733dcde3970bb34062af14fe31c1
Liam Sutton <devliam90@gmail.com>
2020-07-02 11:27:26 +0100

    express server, remove edit link from header

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'build');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log('server listening on port 3000'));
